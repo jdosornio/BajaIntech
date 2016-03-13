389d678d40bdff26a4e01e7b1877d11d36f2c9c2
Alex Herrera <herrera.luis@uabc.edu.mx>
2016-03-13 03:03:15 -0700

    Se anadio metodo para la busqueda de calle y colonia por medio de coordenadas (lat, lng)

--- src/java/com/bajaintec/util/GeocoderUtil.java
+++ src/java/com/bajaintec/util/GeocoderUtil.java
@@ -8,7 +8,10 @@ package com.bajaintec.util;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Arrays;
 
 /**
  *
@@ -16,20 +19,23 @@ import java.io.IOException;
  */
 public class GeocoderUtil {
     
-    public String getCalle() throws Exception{
-        GeocodingResult[] results = {};
+    public static final GeoApiContext CONTEXT = 
+            new GeoApiContext().setApiKey("AIzaSyC2GG9X_PmqhTdWAE1_qBGeEf07FDHiP8A");
+    
+    public String[] getCalleColonia(BigDecimal lat, BigDecimal lng) throws Exception{
+        String[] calleColonia = new String[2];
         try{
-            GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyC2GG9X_PmqhTdWAE1_qBGeEf07FDHiP8A");
-            results =  GeocodingApi.geocode(context,
-                "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
-            System.out.println(results[0].formattedAddress);
+            LatLng coords = new LatLng(lat.doubleValue(), lng.doubleValue());
+            GeocodingResult[] results =  GeocodingApi.reverseGeocode(CONTEXT, coords).await();
+            calleColonia[0] = results[0].addressComponents[1].longName;
+            calleColonia[1] = results[0].addressComponents[2].longName;
         } catch (IOException e){
             e.printStackTrace();
         }
-        return results[0].formattedAddress;
+        return calleColonia;
     }
     
     public static void main(String[] args) throws Exception {
-        new GeocoderUtil().getCalle();
+        new GeocoderUtil().getCalleColonia(new BigDecimal(32.595081), new BigDecimal(-115.482477));
     }
 }
