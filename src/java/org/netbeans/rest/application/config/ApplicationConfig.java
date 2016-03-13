e38073a288c17f603f04a39f17529b9214d300bd
jdosornio <donaldo.osornio@uabc.edu.mx>
2016-03-13 12:53:29 -0700

    Commit licencia gnu en xml

--- src/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ src/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -1,5 +1,5 @@
 /**
- * <!--Copyright(C) <2016> <BajaInTec>
+ * Copyright(C) <2016> <BajaInTec>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,7 +11,7 @@
  * GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.-->
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 package org.netbeans.rest.application.config;
 
